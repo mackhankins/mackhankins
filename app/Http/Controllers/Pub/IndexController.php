78a99f29642ef17d1337259d24603a6c0c16201b
mackhankins <mack.hankins@gmail.com>
2015-05-06 22:25:09 -0500

    completely remove Meta reference

--- app/Http/Controllers/Pub/IndexController.php
+++ app/Http/Controllers/Pub/IndexController.php
@@ -6,7 +6,6 @@ namespace MH\Http\Controllers\Pub;
 
 use MH\Http\Controllers\Controller;
 use MH\Repositories\PostRepositoryInterface;
-use Meta;
 
 class IndexController extends Controller {
 
@@ -25,9 +24,6 @@ class IndexController extends Controller {
      */
     public function index()
     {
-        Meta::meta('title', 'Mack Hankins');
-        Meta::meta('description', 'Php Developer');
-        Meta::meta('image', asset('images/home-logo.png'));
         $posts = $this->post->limit(10)->where('type', '=', 'post')->where('status', '=', 'published')->get();
         $links = $this->post->limit(6)->where('type', '=', 'link')->where('status', '=', 'published')->get();
 
