b836f0bdd1191cf7e3987dbae32b16407328e994
Laravel Shift <shift@laravelshift.com>
2016-06-22 19:38:33 -0400

    Shift cleanup

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace MH\Http\Middleware;
 
 use Illuminate\Cookie\Middleware\EncryptCookies as BaseEncrypter;
 
--- app/Jobs/Job.php
+++ app/Jobs/Job.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Jobs;
+namespace MH\Jobs;
 
 use Illuminate\Bus\Queueable;
 
