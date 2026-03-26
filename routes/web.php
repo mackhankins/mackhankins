2d99e3cc2bc35ac872ed747bdf7b315cbdc31621
Mack Hankins <mack.hankins@gmail.com>
2026-03-26 16:27:03 -0500

    torchlight

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,16 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
+use Torchlight\Middleware\RenderTorchlight;
 
 Route::get('/', HomeController::class)->name('home');
 
 Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
-Route::get('/projects/{project:slug}', [ProjectController::class, 'show'])->name('projects.show');
+Route::get('/projects/{project:slug}', [ProjectController::class, 'show'])->name('projects.show')
+    ->middleware(RenderTorchlight::class);
 
 Route::get('/blog', [PostController::class, 'index'])->name('blog.index');
-Route::get('/blog/{post:slug}', [PostController::class, 'show'])->name('blog.show');
+Route::get('/blog/{post:slug}', [PostController::class, 'show'])->name('blog.show')
+    ->middleware(RenderTorchlight::class);
 
 Route::view('/about', 'about')->name('about');
